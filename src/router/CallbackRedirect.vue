0401098a8ca45eb84dbd6967ace519652c7e56e8
Lucas Williams <lucasnwilliams2008@gmail.com>
2026-01-04 19:20:49 -0500

    Explains callback redirect usage
    Adds a comment explaining the use case of the /callback
    redirect route for Spotify authentication.

--- src/router/CallbackRedirect.vue
+++ src/router/CallbackRedirect.vue
@@ -9,8 +9,9 @@ import {GetSpotifyAuthorization} from "@/API/spotifyAuthentication";
 
 const router = useRouter()
 
-onMounted(async () => {
+// Used for Spotify authentication as it needs to redirect to a /callback url
 
+onMounted(async () => {
   await GetSpotifyAuthorization()
     .then(() => {
       router.push('/')
